37313e9254af6b430211974a6bd4f7730ff1239b
jdegand <70610011+jdegand@users.noreply.github.com>
2024-09-06 20:29:32 -0400

    fix: don't save list to variable

--- controllers/listController.js
+++ controllers/listController.js
@@ -11,20 +11,20 @@ const getLists = async (req, res) => {
         process.env.ACCESS_TOKEN_SECRET,
         async (err, decoded) => {
 
-        try {
-        const list = await List.find({}).populate('user').sort({updatedAt: -1}) //.select('-items')
-        if(!list) return res.status(204).json({ 'message': "No Lists found" });
-        
-        const userLists = list.filter(el => el.user.id === decoded.UserInfo._id)
+            try {
+                const list = await List.find({}).populate('user').sort({ updatedAt: -1 }) //.select('-items')
+                if (!list) return res.status(204).json({ 'message': "No Lists found" });
 
-        res.json(userLists);
-        } catch(err){
-            console.log(err)
-        }
-    })
+                const userLists = list.filter(el => el.user.id === decoded.UserInfo._id)
+
+                res.json(userLists);
+            } catch (err) {
+                console.log(err)
+            }
+        })
 }
 
-const createList = async (req,res) => {
+const createList = async (req, res) => {
 
     const accessToken = req.headers.authorization.split(' ')[1];
 
@@ -33,27 +33,27 @@ const createList = async (req,res) => {
         process.env.ACCESS_TOKEN_SECRET,
         async (err, decoded) => {
 
-            const { listName, state, items } = req.body; 
+            const { listName, state, items } = req.body;
             //if (!listName) return res.status(400).json({ 'message': 'Missing name for List' });
-        
+
             const duplicate = await List.findOne({ listName: listName }).exec()
-        
-            if (duplicate) return res.status(409).json({message: "List name taken"})
-        
+
+            if (duplicate) return res.status(409).json({ message: "List name taken" })
+
             try {
 
-                const result = List.create({ 
+                List.create({
                     "listName": listName,
                     "state": state,
                     "user": decoded.UserInfo._id,
                     "items": items,
                 });
-                
+
                 res.status(201).json({ 'success': `New List added!` });
             } catch (err) {
                 res.status(500).json({ 'message': err.message });
             }
-         
+
         }
     );
 }
@@ -67,18 +67,18 @@ const getListById = async (req, res) => {
         process.env.ACCESS_TOKEN_SECRET,
         async (err, decoded) => {
 
-        try {
-            if (!req?.params?.id) return res.status(400).json({ "message": 'List ID required' });
+            try {
+                if (!req?.params?.id) return res.status(400).json({ "message": 'List ID required' });
 
-            const list = await List.findOne({ _id: req.params.id }).exec();
-            if (!list) {
-                return res.status(204).json({ 'message': `List ID ${req.params.id} not found` });
+                const list = await List.findOne({ _id: req.params.id }).exec();
+                if (!list) {
+                    return res.status(204).json({ 'message': `List ID ${req.params.id} not found` });
+                }
+                res.json(list);
+            } catch (err) {
+                res.status(500).json({ 'message': err.message });
             }
-            res.json(list);
-        } catch(err){
-            res.status(500).json({ 'message': err.message });
-        }
-    })
+        })
 }
 
 const getListByListId = async (req, res) => {
@@ -90,18 +90,18 @@ const getListByListId = async (req, res) => {
         process.env.ACCESS_TOKEN_SECRET,
         async (err, decoded) => {
 
-        try {
-            if (!req?.params?.id) return res.status(400).json({ "message": 'List ID required' });
+            try {
+                if (!req?.params?.id) return res.status(400).json({ "message": 'List ID required' });
 
-            const list = await List.findOne({ _id: req.params.id }).exec();
-            if (!list) {
-                return res.status(204).json({ 'message': `List ID ${req.params.id} not found` });
+                const list = await List.findOne({ _id: req.params.id }).exec();
+                if (!list) {
+                    return res.status(204).json({ 'message': `List ID ${req.params.id} not found` });
+                }
+                res.json(list);
+            } catch (err) {
+                res.status(500).json({ 'message': err.message });
             }
-            res.json(list);
-        } catch(err){
-            res.status(500).json({ 'message': err.message });
-        }
-    })
+        })
 }
 
 const updateList = async (req, res) => {
@@ -114,27 +114,27 @@ const updateList = async (req, res) => {
         async (err, decoded) => {
 
             const { itemIds } = req.body;
-        
+
             const listExists = await List.findOne({ listName: listName }).exec()
-        
-            if (!listExists) return res.status(409).json({message: "List not found"})
-  
+
+            if (!listExists) return res.status(409).json({ message: "List not found" })
+
             try {
                 if (!req?.params?.id) return res.status(400).json({ "message": 'List ID required' });
-                
+
                 const list = await List.findOne({ _id: req.params.id }).exec();
                 if (!list) {
                     return res.status(204).json({ 'message': `List ID ${req.params.id} not found` });
                 }
 
                 list.items = [] // reset and then update
-        
+
                 list.items.push(itemIds)
 
                 await list.save()
-        
+
                 res.json(list);
-            } catch(err){
+            } catch (err) {
                 res.status(500).json({ 'message': err.message });
             }
         }
@@ -148,5 +148,5 @@ module.exports = {
     getListById,
     getListByListId,
     createList,
-    updateList, 
+    updateList,
 }
